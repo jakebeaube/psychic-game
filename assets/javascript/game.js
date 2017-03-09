22d82ae9249987a8c5f3f698b2f73596235003c2
Jake Beaube <Jake Beaube>
2017-03-09 18:29:16 -0500

    1st update

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var thenumber = Math.floor(Math.random()*100+1);
+var numGuesses = 8;
+var wins = 0
+var losses = 0
+
+function checkguess() 
+{
+	var guess = document.getElementById('guess');
+	var output = document.getElementById('output');
+	var myNumber = guess.value;
+
+		numGuesses--;
+	
+	if (myNumber < thenumber) 
+	{
+		output.value = "Too Low!";
+	}
+	else if (myNumber > thenumber) 
+	{
+		output.value = "Too High!";
+	}
+	else if (myNumber == thenumber) 
+	{
+		output.value = "You Got It In Only "+numGuesses+" Tries! Play Again?";
+		alert("The Number Was "+ thenumber +"!");
+		thenumber = Math.floor(Math.random()*100+1);
+		numGuesses = 0;
+		wins++
+		document.getElementById('wins');
+	}
+	
+	
+
+
+
+	
+
+
+
+
+	
+
+	// do not redeclare here with the 'var'.
+	// scope issues we'll talk about tonight...
+	
+}
+
+
+
+
+
